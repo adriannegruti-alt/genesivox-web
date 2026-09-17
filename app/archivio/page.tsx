aeff6080e271729737b5a3a7852a5f4f8c56dc82
adriannegruti-alt <adrian.negruti@gmail.com>
2026-09-17 21:09:15 +0200

    Create page.tsx

---
+++ app/archivio/page.tsx
@@ -0,0 +1,9 @@
+"use client";
+
+export default function ArchivioPage() {
+  return (
+    <div style={{ padding: 24, fontFamily: "sans-serif", color: "#666" }}>
+      <p>Seleziona un cantiere archiviato dalla sidebar a sinistra per aprirlo.</p>
+    </div>
+  );
+}
